689afe5190cfa0a3e73fa22a3b25104d2be23649
Daniel Kessel <d.kessel@gmx.de>
2015-03-23 22:15:26 +0100

    implement file scanning using pydicom, build patient->study->series->instance hierarchy, display summary of scan result

--- pydicomtools/importer/importerapp.py
+++ pydicomtools/importer/importerapp.py
@@ -1,14 +1,75 @@
+import os
+import dicom
+
 
 class ImporterApp():
-
     def __init__(self):
         self.directory = None
+        self.patientIDs = set()
+        self.studyUIDs = dict()
+        self.seriesUIDs = dict()
+        self.instanceUIDs = dict()
+        self.files = dict()
+        self.callback = None
 
     def set_directory(self, selected_dir):
         self.directory = selected_dir
 
+    def get_directory(self):
+        return self.directory
+
     def get_directory_content_summary(self):
         if self.directory:
-            return "Fake summary here..."
+            if len(self.patientIDs) > 0:
+                total_size = 0
+                for f in self.files.values():
+                    total_size += os.path.getsize(f)
+                total_size /= 1000000
+                total_size = round(total_size)
+
+                return "Patients: " + str(len(self.patientIDs)) + "\n" \
+                        + "Studies: " + str(len(self.studyUIDs)) + "\n" \
+                        + "Series: " + str(len(self.seriesUIDs)) + "\n" \
+                        + "Instances: " + str(len(self.instanceUIDs)) + "\n" \
+                        + "Total size: " + str(total_size) + " MB"
+
+            else:
+                return "Scanning ..."
         else:
-            return "No directory selected"
+            return "No DICOM data found"
+
+    def set_scan_callback(self, callback):
+        self.callback = callback
+
+    def scan_directory(self):
+        if self.directory:
+            dir_content = os.listdir(self.directory)
+            # only try files
+            filter(os.path.isfile, dir_content)
+            # sort by name
+            dir_content.sort()
+
+            for file in dir_content:
+                file = os.path.join(self.directory, file)
+                dcm = dicom.read_file(file)
+                # Patient ID
+                pid = dcm.PatientID
+                if not pid in self.patientIDs:
+                    self.patientIDs.add(pid)
+                # Study Instance UID
+                study_uid = dcm.StudyInstanceUID
+                if not study_uid in self.studyUIDs:
+                    self.studyUIDs[study_uid] = pid
+                # Series Instance UID
+                series_uid = dcm.SeriesInstanceUID
+                if not series_uid in self.seriesUIDs:
+                    self.seriesUIDs[series_uid] = study_uid
+                # SOP Instance UID
+                instance_uid = dcm.SOPInstanceUID
+                if not instance_uid in self.instanceUIDs:
+                    self.instanceUIDs[instance_uid] = series_uid
+                    self.files[instance_uid] = file
+
+        # tell callback that we are done
+        if self.callback:
+            self.callback()
--- pydicomtools/importer/tkinter/importerframe.py
+++ pydicomtools/importer/tkinter/importerframe.py
@@ -1,6 +1,6 @@
-
 import tkinter
 import tkinter.filedialog
+import time
 from pydicomtools.importer.importerapp import ImporterApp
 
 
@@ -8,19 +8,19 @@ class ImporterFrame(tkinter.Frame):
     def __init__(self, app, master=None):
         tkinter.Frame.__init__(self, master)
         self.app = app
-        
-        self.pack()
+
+        self.pack(padx=10, pady=10)
 
         self.topText = tkinter.StringVar()
         self.topText.set("Where is the data you want to import?")
 
         self.topLabel = tkinter.Label(self, textvariable=self.topText)
-        self.topLabel.pack()
+        self.topLabel.pack(fill="both")
 
         self.selectDirectoryButton = tkinter.Button(self)
         self.selectDirectoryButton["text"] = "in a directory..."
         self.selectDirectoryButton["command"] = self.select_directory
-        self.selectDirectoryButton.pack()
+        self.selectDirectoryButton.pack(padx=5, pady=5)
 
         self.backButton = tkinter.Button(self)
         self.backButton["text"] = "< Back"
@@ -71,13 +71,14 @@ class ImporterFrame(tkinter.Frame):
         self.sendDataButton["text"] = "Send data"
 
     def select_directory(self):
-        selected_dir = tkinter.filedialog.askdirectory(mustexist=True, title="select the directory to import from")
+        selected_dir = tkinter.filedialog.askdirectory(mustexist=True, title="select the directory to import from",
+                                                       initialdir="/home/daniel/Downloads/DICOM/case1/")
         if selected_dir:
             self.app.set_directory(selected_dir)
             # TODO: start background scan of data... show text 'Searching for data'
-            self.go_to_step_two()
+            self.go_to_step_two(scan=True)
 
-    def go_to_step_two(self):
+    def go_to_step_two(self, scan=False):
         # remove elements
         # from step 1
         self.selectDirectoryButton.pack_forget()
@@ -91,11 +92,23 @@ class ImporterFrame(tkinter.Frame):
         self.qrVerifyCheckButton.pack_forget()
         self.sendDataButton.pack_forget()
         # add elements
-        self.topText.set('Summary of data to import:\n\n' + self.app.get_directory_content_summary())
         self.backButton["command"] = self.forget_directory_and_go_to_step_one
-        self.backButton.pack()
-        self.addMoreButton.pack()
-        self.continueButton.pack()
+        self.backButton.pack(padx=5, pady=5)
+        self.addMoreButton.pack(padx=5, pady=5, after=self.backButton)
+        self.continueButton.pack(padx=5, pady=5, after=self.addMoreButton)
+        # start scanning...
+        if scan:
+            self.continueButton["state"] = "disabled"
+            self.app.set_scan_callback(self.update_result)
+            self.app.scan_directory()
+            self.topText.set('Summary of data to import:\n\n' + self.app.get_directory_content_summary())
+        else:
+            self.continueButton["state"] = "normal"
+            self.topText.set('Summary of data to import:\n\n' + self.app.get_directory_content_summary())
+
+    def update_result(self):
+        self.topText.set('Summary of data to import:\n\n' + self.app.get_directory_content_summary())
+        self.continueButton["state"] = "normal"
 
     def forget_directory_and_go_to_step_one(self):
         self.app.set_directory(None)
@@ -109,7 +122,7 @@ class ImporterFrame(tkinter.Frame):
         self.continueButton.pack_forget()
         # add elements
         self.topText.set("Where is the data you want to import?")
-        self.selectDirectoryButton.pack()
+        self.selectDirectoryButton.pack(padx=5, pady=5)
 
     def go_to_step_three(self):
         # remove elements
@@ -122,13 +135,13 @@ class ImporterFrame(tkinter.Frame):
         self.networkAddressEntry.pack()
         self.cStorePortLabel.pack()
         self.cStorePortEntry.pack()
-        self.cEchoCheckButton.pack()
-        self.dicomCommitmentCheckButton.pack()
-        self.qrVerifyCheckButton.pack()
+        self.cEchoCheckButton.pack(padx=5, pady=5)
+        self.dicomCommitmentCheckButton.pack(padx=5, pady=5)
+        self.qrVerifyCheckButton.pack(padx=5, pady=5)
 
         self.backButton["command"] = self.go_to_step_two
-        self.backButton.pack()
-        self.sendDataButton.pack()
+        self.backButton.pack(padx=5, pady=5)
+        self.sendDataButton.pack(padx=5, pady=5)
 
 
 def main():
@@ -138,6 +151,7 @@ def main():
     app = ImporterApp()
     frame = ImporterFrame(app, root)
     frame.mainloop()
+
 
 if __name__ == "__main__":
     main()
